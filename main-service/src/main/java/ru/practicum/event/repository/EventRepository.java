481842552503380ae6c6088f6344c2ffed4197e9
Daniil <rusbear060@gmail.com>
2023-07-25 13:50:31 +0300

    init commit

--- main-service/src/main/java/ru/practicum/event/repository/EventRepository.java
+++ main-service/src/main/java/ru/practicum/event/repository/EventRepository.java
@@ -3,6 +3,7 @@ package ru.practicum.event.repository;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import ru.practicum.event.enums.State;
 import ru.practicum.event.model.Event;
@@ -17,45 +18,43 @@ public interface EventRepository extends JpaRepository<Event, Long> {
 
     List<Event> findAllByIdIn(List<Long> eventId);
 
-    @Query(value = "SELECT e " +
+    @Query("SELECT e " +
             "FROM Event AS e " +
             "WHERE " +
             "(" +
-            "?1 IS NULL " +
-            "OR LOWER(e.description) LIKE CONCAT('%', ?1, '%') " +
-            "OR LOWER(e.annotation) LIKE CONCAT('%', ?1, '%')" +
+            ":text IS NULL " +
+            "OR LOWER(e.description) LIKE CONCAT('%', :text, '%') " +
+            "OR LOWER(e.annotation) LIKE CONCAT('%', :text, '%')" +
             ")" +
-            "AND (?2 IS NULL OR e.state IN (?2)) " +
-            "AND (?3 IS NULL OR e.category.id IN (?3)) " +
-            "AND (?4 IS NULL OR e.paid = ?4) " +
-            "AND (CAST(?5 AS date) IS NULL OR e.eventDate >= ?5) " +
-            "AND (CAST(?6 AS date) IS NULL OR e.eventDate <= ?6) " +
-            "order by e.eventDate",
-    nativeQuery = true)
+            "AND (:states IS NULL OR e.state IN (:states)) " +
+            "AND (:categories IS NULL OR e.category.id IN (:categories)) " +
+            "AND (:paid IS NULL OR e.paid = :paid) " +
+            "AND (CAST(:rangeStart AS date) IS NULL OR e.eventDate >= :rangeStart) " +
+            "AND (CAST(:rangeEnd AS date) IS NULL OR e.eventDate <= :rangeEnd) " +
+            "order by e.eventDate")
     List<Event> findByParamsOrderByDate(
-            String text,
-            List<State> states,
-            List<Long> categories,
-            Boolean paid,
-            LocalDateTime rangeStart,
-            LocalDateTime rangeEnd,
+            @Param("text") String text,
+            @Param("states") List<State> states,
+            @Param("categories") List<Long> categories,
+            @Param("paid") Boolean paid,
+            @Param("rangeStart") LocalDateTime rangeStart,
+            @Param("rangeEnd") LocalDateTime rangeEnd,
             Pageable pageable);
 
-    @Query(value = "SELECT e " +
+    @Query("SELECT e " +
             "FROM Event AS e " +
-            "WHERE (?1 IS NULL OR e.initiator.id IN (?1)) " +
-            "AND (?2 IS NULL OR e.state IN (?2)) " +
-            "AND (?3 IS NULL OR e.category.id IN (?3)) " +
-            "AND (CAST(?4 AS date) IS NULL OR e.eventDate >= ?4) " +
-            "AND (CAST(?5 AS date) IS NULL OR e.eventDate <= ?5) " +
-            "order by e.eventDate",
-            nativeQuery = true)
+            "WHERE (:users IS NULL OR e.initiator.id IN (:users)) " +
+            "AND (:states IS NULL OR e.state IN (:states)) " +
+            "AND (:categories IS NULL OR e.category.id IN (:categories)) " +
+            "AND (CAST(:rangeStart AS date) IS NULL OR e.eventDate >= :rangeStart) " +
+            "AND (CAST(:rangeEnd AS date) IS NULL OR e.eventDate <= :rangeEnd) " +
+            "order by e.eventDate")
     List<Event> findByParams(
-            List<Long> users,
-            List<State> states,
-            List<Long> categories,
-            LocalDateTime rangeStart,
-            LocalDateTime rangeEnd,
+            @Param("users") List<Long> users,
+            @Param("states") List<State> states,
+            @Param("categories") List<Long> categories,
+            @Param("rangeStart") LocalDateTime rangeStart,
+            @Param("rangeEnd") LocalDateTime rangeEnd,
             Pageable pageable);
 
     List<Event> findAllByCategoryId(Long catId);
